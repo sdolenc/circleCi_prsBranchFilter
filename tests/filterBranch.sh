6dfd932c9e7682ec1dc34bd3c489839e134f546d
localstepdo <stepdo@microsoft.com>
2018-03-06 18:07:30 -0800

    workaround

--- tests/filterBranch.sh
+++ tests/filterBranch.sh
@@ -1,4 +1,69 @@
 #!/usr/bin/env bash
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 
-#todo
+set -ex
+
+get_current_branch()
+{
+    local branchInfo=
+
+    if [[ -n $CIRCLE_BRANCH ]] ; then
+        branchInfo=$CIRCLE_BRANCH
+    else
+        # Current branch is prefixed with an asterisk. Remove it.
+        local prefix='* '
+        branchInfo=$(git branch | grep "$prefix" | sed "s/$prefix//g")
+    fi
+
+    echo "$branchInfo"
+}
+
+get_base_branch()
+{
+    if [[ -n $CIRCLE_PULL_REQUEST ]] ; then
+        # Construct github API url
+        local g="github.com"
+        local apiURL=$(echo "$CIRCLE_PULL_REQUEST" | sed "s#/$g/#/api.$g/repos/#g" | sed "s#/pull/#/pulls/#g") > /dev/null
+        local baseBranch=$(curl -sSl $apiURL | jq -r '.base.ref')
+    fi
+}
+
+is_valid_branch()
+{
+    local branch=$1
+
+    # Is branch useful?
+    [[ -n "$branch" ]] && [[ $branch != null ]] && [[ $branch != *"no branch"* ]] && [[ $branch != *"detached"* ]]
+}
+
+branch_in_list()
+{
+    local branch=$1
+
+    for filter in $ONLY_BRANCHES; do
+        if [[ $branch == $filter ]] ; then
+            true
+        fi
+    done
+
+    false
+}
+
+sudo apt update
+sudo apt instally -y jq curl
+
+current_branch=$(get_current_branch)
+if [[ is_valid_branch $current_branch ]] ; then
+    if [[ branch_in_list $current_branch ]] ; then
+        exit 0
+    fi
+fi
+
+base_branch=$(get_base_branch)
+if [[ is_valid_branch $base_branch ]] ; then
+    if [[ branch_in_list $base_branch ]] ; then
+        exit 0
+    fi
+fi
+
+exit 1
